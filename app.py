bf2d968bc5cdbf7e8545104fff144f36dd22fdfb
n <n@pop-os.localdomain>
2023-07-28 20:17:23 -0600

    deployment

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ def get_shortest_route(starting_address, waypoints):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
